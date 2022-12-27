2cf5a60190afbd304f41b950cb818fa1b7cccc41
NaidaNozic <92310390+NaidaNozic@users.noreply.github.com>
2022-12-27 14:12:53 +0100

    Logout radi

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const session = require("express-session");
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const app = express();
+const app = express()
+const path = require('path')
 
+//app.set("view engine", "pug");
+//app.set("views", path.join(__dirname, "views"))
 app.use(session({
-    secret: 'ovdje ide username nastavnika',
+    secret: 'neka tajna sifra',
     resave: true,
     saveUninitialized: true
     }))
-    
-const path = require('path')
 app.use(bodyParser.json())
 
 //_dirname allows us to get the root directory of our project
@@ -29,7 +30,11 @@ app.get('/predmet.html', (req, res) => {
     res.sendFile(path.join(__dirname,'public','html','predmet.html'));
 });
 
-app.get('/login',(req,res) => {
+app.get('/predmeti.html', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','html','predmeti.html'));
+});
+
+app.get('/prijava.html',(req,res) => {
     res.sendFile(path.join(__dirname,'public','html','prijava.html'));
 })
 
@@ -43,19 +48,33 @@ app.post('/login', (req, res) => {
         var currentNastavnik=nastavnici.find(element => element.nastavnik.username == jsonObj.username 
                                                         && element.nastavnik.password_hash==jsonObj.password)                              
         if(currentNastavnik!=null){
-            res.end(JSON.stringify({poruka: 'Uspješna prijava'}))
+            //u sesiju upisujem username i listu predmeta na kojima je nastavnik
+            req.session.username=currentNastavnik.nastavnik.username
+            req.session.predmeti=currentNastavnik.predmeti
+
+            res.end(JSON.stringify({poruka: 'Uspješna prijava', redirect_path: "/predmeti.html"}))
         }else{
             res.end(JSON.stringify({poruka: 'Neuspješna prijava'}))
         }
     })
- })  
+})  
+
+app.post('/logout', (req,res) => {
+    //brise podatke iz sesije
+    if(req.session.username!=null){
+        console.log("Username prije brisanja: "+req.session.username)
+        console.log("Predmeti prije brisanja: "+req.session.predmeti)
+        req.session.username=null
+        req.session.predmeti=null
+    }
+    res.end(JSON.stringify({poruka: 'Uspješan logout', redirect_path: "/prijava.html"}))
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/css', express.static(path.join(__dirname)))
 app.use('/html', express.static(path.join(__dirname)))
 app.use('/scripts', express.static(path.join(__dirname)))
 app.use('/img', express.static(path.join(__dirname)))
-
 
 app.listen(3000, ()=> {
     console.log("Server is running at port 3000...")
--- public/scripts/poziviAjax.js
+++ public/scripts/poziviAjax.js
@@ -1,5 +1,5 @@
-let posaljiPodatke= function(objekat,funckija){
-        var ajax = new XMLHttpRequest();
+let posaljiPodatke = function(objekat,funckija){
+        var ajax = new XMLHttpRequest()
 
         ajax.onreadystatechange = function() {
             if (ajax.readyState == 4 && ajax.status == 200){
@@ -14,5 +14,39 @@ let posaljiPodatke= function(objekat,funckija){
         ajax.setRequestHeader("Content-Type", "application/json")
         forSend=JSON.stringify(objekat)
         ajax.send(forSend)
+}
 
+let login = function(objectUsername,objectPassword){
+    
+    var obj = new Object()
+    obj.username = objectUsername.value
+    obj.password= objectPassword.value
+    
+    posaljiPodatke(obj, function(err,data){
+
+        if(err != null){
+            window.alert(err)
+        }else{
+            json=JSON.parse(data)
+            //redirektujem se na stranicu predmeti.html
+            window.location.href=json.redirect_path
+        }
+    })
+}
+
+let logoutFunction = function(){
+    var ajax = new XMLHttpRequest()
+    ajax.onreadystatechange = function() {
+        if (ajax.readyState == 4 && ajax.status == 200){
+            json=JSON.parse(ajax.response)
+            //redirektujem se nazad na pocetnu stranicu prijava.html
+            window.location.href=json.redirect_path
+        }
+        else if(ajax.readyState == 4){
+            //desio se neki error
+            console.log(ajax.statusText)
+        }
+    }
+    ajax.open("POST", "http://localhost:3000/logout", true)
+    ajax.send()
 }
